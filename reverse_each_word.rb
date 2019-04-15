10dbd9665ae470cc2ebc2d30400fed26ca68d5df
Carla Stickler <carlastickler@gmail.com>
2019-04-15 16:10:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence)
   new_sentence = []
-  sentence1.each do |word|
+  sentence.
+  sentence.each do |word|
     new_sentence.push(word.reverse)
   end
   new_sentence
