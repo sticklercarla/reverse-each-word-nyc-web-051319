2797247cfc344aa789071cf17361464f7f20ca2c
Carla Stickler <carlastickler@gmail.com>
2019-04-15 16:47:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
-def reverse_each_word(sentence)
-   new_sentence = []
-   array_sentence = []
-   array_sentence = sentence.split(' ')
-   array_sentence.each do |word|
-     new_sentence.push(word.reverse)
-    end
-   new_sentence.join(' ')
- end
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   array_sentence = []
+#   array_sentence = sentence.split(' ')
+#   array_sentence.each do |word|
+#     new_sentence.push(word.reverse)
+#     end
+#   new_sentence.join(' ')
+# end
  
- def reverse_each_word_with_collect(sentence)
+ def reverse_each_word(sentence)
    sentence.split.collect {|word| word.reverse}.join(' ')
   # new_sentence = []
   # new_sentence = sentence.split(' ')
